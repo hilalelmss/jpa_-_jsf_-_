3b0f51198f6203f27cb227a079f321439b1527e1
hilalelmss <hilal.elmss@gmail.com>
2017-09-22 01:41:53 +0300

    proje bitiş

--- PersonelHaberYonetimi/src/com/hilalelmas/controller/Islem.java
+++ PersonelHaberYonetimi/src/com/hilalelmas/controller/Islem.java
@@ -2,7 +2,7 @@ package com.hilalelmas.controller;
 
 public class Islem {
 	public static void main(String[] args) {
-		System.out.println("hello jsf");
 		System.out.println("-----");
+		System.out.println("hello jsf");
 	}
 }
